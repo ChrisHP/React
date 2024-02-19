db1d25e68a262c7a0b160946c79540a7f9963eb4
Christian <christianalexp@live.com>
2024-02-19 12:15:55 +0100

    Ej2

--- EjerciciosReact/src/HelloWorld.jsx
+++ EjerciciosReact/src/HelloWorld.jsx
@@ -1,9 +1,14 @@
 
-export function HelloWorld(){
-    return <h2>Hello World!</h2>
+export function Hello(){
+    return (
+    <>
+        <h2>Hello World!</h2>
+        <Message/>
+    </>);
 }
 export function App(){
-    return (
-        <div><HelloWorld/></div>
-    )
+    return <div><Hello/></div>;
+}
+export function Message(){
+    return <p>What a beautiful day!</p>
 }
--- EjerciciosReact/src/index.jsx
+++ EjerciciosReact/src/index.jsx
@@ -1,4 +1,4 @@
-import { HelloWorld,App } from "./HelloWorld";
+import { Hello,App,Message } from "./HelloWorld";
 import {createRoot} from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(<App/>);
