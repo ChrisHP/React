02c138bd4f5bb2638dce26dfb6e2f1fe09a555b1
Christian <christianalexp@live.com>
2024-02-20 11:18:19 +0100

    Ej6

--- EjerciciosReact/src/HelloWorld.jsx
+++ EjerciciosReact/src/HelloWorld.jsx
@@ -1,9 +1,16 @@
-
-export function Welcome({name="Usuario desconocido"}){
-    return (
-        <p>Welcome, {name}!</p>
-    );
+export function Welcome({ name = "Usuario desconocido", age }) {
+  let strongAge = <strong>{age}</strong>;
+  return (
+    <>
+      <p>Welcome, {name}!</p>
+      <p>Your age is {strongAge}</p>
+    </>
+  );
 }
-export function App(){
-    return <div> <Welcome name="Pepe"/></div>;
+export function App() {
+  return (
+    <div>
+      <Welcome name="Pepe" age={15} />
+    </div>
+  );
 }
