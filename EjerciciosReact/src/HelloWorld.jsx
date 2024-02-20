9de93528f14e3481a73287e231448e0ed9ed73c0
Christian <christianalexp@live.com>
2024-02-20 11:08:55 +0100

    Ej5

--- EjerciciosReact/src/HelloWorld.jsx
+++ EjerciciosReact/src/HelloWorld.jsx
@@ -1,15 +1,9 @@
-//Si, se puede, lo renderiza las mismas veces
-//Si, se puede, lo renderiza de manera normal
-export function Hello(){
+
+export function Welcome({name="Usuario desconocido"}){
     return (
-    <>
-        <h2>Hello World!</h2>
-        <Message/>
-    </>);
+        <p>Welcome, {name}!</p>
+    );
 }
 export function App(){
-    return <div><Hello/><Hello/><Hello/><Hello/><Message/></div>;
-}
-export function Message(){
-    return <p>What a beautiful day!</p>
+    return <div> <Welcome name="Pepe"/></div>;
 }
--- EjerciciosReact/src/index.jsx
+++ EjerciciosReact/src/index.jsx
@@ -1,4 +1,4 @@
-import { Hello,App,Message } from "./HelloWorld";
+import { App,Welcome } from "./HelloWorld";
 import {createRoot} from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(<App/>);
